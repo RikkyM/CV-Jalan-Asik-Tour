f06ebf3ef30a5e22624ef91e8809b34b9f0d0a81
RikkyM <rikky.mahendra54@gmail.com>
2026-01-28 06:37:41 +0700

    changes: sidebar ui user login

--- backend/database/migrations/2025_11_17_123426_create_template_surat_table.php
+++ backend/database/migrations/2025_11_17_123426_create_template_surat_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('kode_surat');
             $table->string('file_surat');
+            $table->string('type');
             $table->timestamps();
         });
     }
